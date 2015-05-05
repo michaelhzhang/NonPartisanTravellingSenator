56cae0e0181432da51d651a53d2fb6c79b084c0f
MichaelHZhang <michael.zhang@berkeley.edu>
2015-05-05 15:23:10 -0700

    Renamed NPTSP_ant_code to code. Code to run on all isntances in compute_paths.py. Refactored ant colonization to callable API

---
+++ code/ant_nptsp_runner.py
@@ -0,0 +1,68 @@
+from ant_colony import AntColony
+from ant_graph import AntGraph
+
+import sys
+import traceback
+
+# NOTE: CITIES ARE ASSUMED TO BE 0 INDEXED INTERNALLY. ONLY ON FILE OUTPUT ARE CITIES ONE INDEXED
+
+class AntNPTSPRunner:
+    def __init__(self, num_nodes, distances, colors):
+        """Each instance of AntNPTSPRunner is responsible for running ant colonization on one input.
+        Constructor takes in the number of nodes, matrix of distances and string of colors."""
+        self.num_nodes = num_nodes
+        self.distances = distances
+        self.colors = colors
+        self.best_path_vec = None
+        self.best_path_cost = None
+        self.best_path_colors = None
+        self.__calculate()
+
+    def __calculate(self):
+        """Private method. Computes an optimal path using ant colonization."""
+        # TODO: Figure out optimal number of ants (research paper suggests this might be 10)
+        if self.num_nodes <= 10:
+            #num_ants = 1 # for testing
+            self.num_ants = 20
+            self.num_iterations = 12
+            self.num_repetitions = 1 # TODO: Increase number of repetitions?
+        else:
+            self.num_ants = 28 
+            self.num_iterations = 30 # TODO: INCREASE THIS?
+            self.num_repetitions = 3 # TODO: INCREASE THIS?
+
+        try:
+            graph = AntGraph(self.num_nodes, self.distances,self.colors) # construct the graph
+            self.best_path_cost = sys.maxint # initialize cost to infinity
+            for i in range(0, self.num_repetitions): # run multiple repetitions. Run multiple repetitions because each iteration is random.
+                graph.reset_tau()  # reset trail level
+                ant_colony = AntColony(graph, self.num_ants, self.num_iterations) # construct ant colony
+                ant_colony.start()
+                if ant_colony.best_path_cost < self.best_path_cost: # if we found a better path, take that path
+                    self.best_path_vec = ant_colony.best_path_vec
+                    self.best_path_cost = ant_colony.best_path_cost
+            self.best_path_colors = [graph.get_color(node) for node in self.best_path_vec]
+
+        except Exception, e:
+            print "exception: " + str(e)
+            traceback.print_exc()
+
+    def get_best_path(self):
+        """Returns vector containing of the best path computed. This is 0 indexed. Raises an exception if calculate()
+        has not yet been called"""
+        if self.best_path_vec == None:
+            raise Exception("No best path yet calculated")
+        return self.best_path_vec
+
+    def get_best_path_cost(self):
+        """Returns cost of the best path computed."""
+        if self.best_path_cost == None:
+            raise Exception("No best path yet calculated")
+        return self.best_path_cost
+
+    def get_best_path_colors(self):
+        """Returns colors of the best path computed. Use this to check validity of paths computed."""
+        if self.best_path_colors == None:
+            raise Exception("No best path yet calculated")
+        return self.best_path_colors
+    
---
+++ code/compute_paths.py
@@ -0,0 +1,78 @@
+from ant_nptsp_runner import AntNPTSPRunner
+from LocalSearch import *
+import os.path
+
+import sys
+import traceback
+
+def process_input(filename):
+    """Opens a file of name "filename" in the instances directory"""
+    fin = open("../instances/" + filename, "r")
+    num_nodes = int(fin.readline())
+    distances = [[] for i in range(num_nodes)]
+    for i in xrange(num_nodes):
+        distances[i] = [int(x) for x in fin.readline().split()]
+    colors = fin.readline().rstrip("\n") # Make sure to remove the \n
+    return (num_nodes,distances,colors)
+
+def score_path(num_nodes,distances,path, one_index = True):
+    """Calculates the cost of path (given as a list of nodes, 1 indexed) given an instances with distances matrix "distances".
+    If one_index is False, assume that the path is zero indexed.
+    """
+    cost = 0
+    for i in xrange(num_nodes - 1):
+        if one_index:
+            cur = path[i]-1
+            next = path[i+1]-1
+        else:
+            cur = path[i]
+            next = path[i+1]
+        cost += distances[cur][next]
+
+    return cost
+
+if __name__ == "__main__":
+    # TODO: Exception handling?
+    NUM_INSTANCES = 1 # Default to 495
+
+    # Check if answer.out already exits
+    prev_answers = None
+    if os.path.isfile("test.out"):
+        prev_answer_file = open("test.out","r")
+        prev_answers =[[] for i in xrange(0,NUM_INSTANCES)]
+        for i in xrange(0,NUM_INSTANCES):
+            prev_answers[i] = [int(x) for x in prev_answer_file.readline().split()]
+
+    # Output file
+    fout = open("test.out","w")
+    for i in xrange(0,NUM_INSTANCES):
+        processed = process_input(str(i+1) + ".in") # Note for testing: 101.in is really small
+        runner = AntNPTSPRunner(num_nodes, distances, colors)
+
+        # print runner.get_best_path()
+        # print runner.get_best_path_cost()
+        # print runner.get_best_path_colors()
+
+        # Output code
+        best_path_vec = runner.get_best_path()
+
+        # Only replace previous answer if new answer is better
+        if prev_answers is not None:
+            #print "Previously computed"
+            prev_cost = score_path(num_nodes,distances,prev_answers[i])
+            #print "prev_cost: " + str(prev_cost)
+            if runner.get_best_path_cost() > prev_cost:
+                assign = prev_answers[i]
+                fout.write("%s\n" % " ".join(map(str, assign)))
+            else:       
+                assign = [0] * num_nodes
+                for j in xrange(num_nodes):
+                    assign[j] = best_path_vec[j] + 1 # 1 indexes the nodes
+            fout.write("%s\n" % " ".join(map(str, assign)))
+        else:       
+            assign = [0] * num_nodes
+            for j in xrange(num_nodes):
+                assign[j] = best_path_vec[j] + 1 # 1 indexes the nodes
+            fout.write("%s\n" % " ".join(map(str, assign)))
+
+    fout.close()

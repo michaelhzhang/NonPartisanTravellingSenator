9db50a4ecac2f1101a385865a64b958046b8875a
MichaelHZhang <michael.zhang@berkeley.edu>
2015-05-05 21:29:43 -0700

    Hardcoded case

--- code/compute_paths.py
+++ code/compute_paths.py
@@ -19,7 +19,7 @@ def process_input(filename):
     return (num_nodes,distances,colors)
 
 def score_path(num_nodes,distances,path, one_index = True):
-    """Calculates the cost of path (given as a list of nodes, 1 indexed) given an instances with distances matrix "distances".
+    """Calculates the cost of path (given as a list of nodes, 1 indexed by default) given an instances with distances matrix "distances".
     If one_index is False, assume that the path is zero indexed.
     """
     cost = 0
@@ -47,33 +47,39 @@ if __name__ == "__main__":
             prev_answers[i] = [int(x) for x in prev_answer_file.readline().split()]
 
     # Output file
-    fout = open("answer.out","w")
+    #fout = open("answer.out","w")
     for i in xrange(0,NUM_INSTANCES):
         num_nodes,distances,colors = process_input(str(i+1) + ".in")
-        # num_nodes, distances, colors = process_input("101.in") # For testing: 101.in is small
+        #num_nodes, distances, colors = process_input("101.in") # For testing: 101.in is small
 
         if num_nodes <= 20: # TODO: What is the upper bound on what DPs can handle?
+            print "DP"
             best_path_vec, best_path_cost = nptsp_dp(num_nodes,distances,colors)
+            print best_path_cost
 
         else:
+            print "Non-DP"
             runner = AntNPTSPRunner(num_nodes,distances,colors)
 
-            print runner.get_best_path()
-            print "Current cost: " + str(runner.get_best_path_cost())
-            print runner.get_best_path_colors()
+            # print runner.get_best_path()
+            # print "Current cost: " + str(runner.get_best_path_cost())
+            # print runner.get_best_path_colors()
 
             # Output code
             best_path_vec = runner.get_best_path()
             best_path_cost = runner.get_best_path_cost()
 
-        
-
+        # Hardcoded cases
+        dumb_guess = [i+1 for i in range(0,num_nodes)]
+        dumb_guess_cost = score_path(num_nodes,distances,dumb_guess)
+        if dumb_guess_cost < best_path_cost:
+            best_path_vec, best_path_vec = dumb_guess, dumb_guess_cost
 
         # Only replace previous answer if new answer is better
         if prev_answers is not None:
             #print "Previously computed"
             prev_cost = score_path(num_nodes,distances,prev_answers[i])
-            print "prev_cost: " + str(prev_cost)
+            # print "prev_cost: " + str(prev_cost)
             if best_path_cost > prev_cost:
                 assign = prev_answers[i]
                 fout.write("%s\n" % " ".join(map(str, assign)))
